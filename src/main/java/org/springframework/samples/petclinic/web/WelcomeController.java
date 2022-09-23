303af83b35ad6302f525bc0014f7af3cd950f417
alejandromd <alepridur@alum.us.es>
2022-09-23 16:07:59 +0200

    practica 1 ejercicio 6

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -1,7 +1,10 @@
 package org.springframework.samples.petclinic.web;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.samples.petclinic.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,7 +14,14 @@ public class WelcomeController {
 	
 	  @GetMapping({"/","/welcome"})
 	  public String welcome(Map<String, Object> model) {	    
-
+		List<Person> persons = new ArrayList<Person>();
+		Person p1 = new Person();
+		p1.setFirstName("Alejandro");
+		p1.setLastName("Medina");
+		persons.add(p1);
+		model.put("persons",persons);
+		model.put("title","Practica 1");
+		model.put("group","estudiante");
 	    return "welcome";
 	  }
 }
