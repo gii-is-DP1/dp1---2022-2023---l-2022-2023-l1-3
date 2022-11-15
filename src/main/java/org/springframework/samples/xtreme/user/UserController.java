7319b6feed4c7bec6686d7a232c2c19e304534ff
goncammej <goncammej@alum.us.es>
2022-11-15 20:10:40 +0100

    Arreglando validación incicio de sesión

--- src/main/java/org/springframework/samples/xtreme/user/UserController.java
+++ src/main/java/org/springframework/samples/xtreme/user/UserController.java
@@ -3,15 +3,21 @@ package org.springframework.samples.xtreme.user;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.xtreme.admin.AdminService;
+import org.springframework.samples.xtreme.player.LoginForm;
 import org.springframework.samples.xtreme.player.Player;
 import org.springframework.samples.xtreme.player.PlayerService;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +25,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -26,20 +33,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class UserController {
     
     private static final String LOGIN_FORM = "users/loginForm";
-    private static final String GAME_HOME = "players/gameHome";
     private static final String LOGOUT = "users/logout";
 
-    private final UserValidator userValidator;
-
-	@Autowired
-	public UserController(UserValidator userValidator) {
-        this.userValidator = userValidator;
-	}
-
-    @InitBinder("user")
+    @InitBinder
     protected void initBinder(WebDataBinder binder) {
-        binder.addValidators(userValidator);
-        //binder.setDisallowedFields("id");
+        binder.setDisallowedFields("id");
     }
 
     @GetMapping("/login")
@@ -49,16 +47,6 @@ public class UserController {
         return mav;
     }
 
-    @PostMapping("/login")
-    public ModelAndView processLoginForm(@Valid @ModelAttribute("user") User user, BindingResult result) {
-        ModelAndView mav = new ModelAndView("redirect:/players/gameHome");
-        if (result.hasErrors()) {
-            mav = new ModelAndView(LOGIN_FORM);
-            mav.addObject("user", user);
-        }
-        return mav;
-    }
-        
     @GetMapping(path = "/logout-screen")
     public ModelAndView userLogout() {
         ModelAndView mav = new ModelAndView(LOGOUT);
