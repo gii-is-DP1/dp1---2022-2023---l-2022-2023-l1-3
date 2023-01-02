12b68dd260e893bf1a0d094c5ec28a7345d3ce0c
goncammej <goncammej@alum.us.es>
2023-01-02 03:49:24 +0100

    Cambios en la entidad ParchisTurn e implementación ParchisTurnService

--- src/main/java/org/springframework/samples/xtreme/board/ParchisController.java
+++ src/main/java/org/springframework/samples/xtreme/board/ParchisController.java
@@ -8,6 +8,8 @@ import org.apache.logging.log4j.spi.ObjectThreadContextMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,10 +21,22 @@ public class ParchisController {
     ParchisBoardService parchisService;
 
     @GetMapping(path = "")
-    public ModelAndView game(HttpServletResponse response) {
+    public ModelAndView gameTest(HttpServletResponse response) {
         response.addHeader("Refresh", "10");
         ModelAndView mav = new ModelAndView("game/parchisBoard");
         mav.addObject("board", parchisService.findById(1).get());
         return mav;
+    }
+
+    @GetMapping(path = "/{id}")
+    public ModelAndView gameRoom(@PathVariable Integer id, HttpServletResponse response) {
+        // TODO: Vista principal del tablero
+        return null;
+    }
+
+    @GetMapping(path = "/{id}/turn")
+    public ModelAndView turnView(@PathVariable Integer id) {
+        // TODO: Vista en la que el jugador lanza dados y elige ficha
+        return null;
     }
 }
